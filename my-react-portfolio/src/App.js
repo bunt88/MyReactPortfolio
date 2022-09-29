883b1bf3c77f0f3daa359973aa0b2778cbf28a8c
unknown <bunt88@gmail.com>
2022-09-29 13:46:22 -0600

    Added About Me Page

--- my-react-portfolio/src/App.js
+++ my-react-portfolio/src/App.js
@@ -1,13 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 // import logo from "./logo.svg";
 import "./App.css";
 
 import Navigation from "./Components/Header";
+import AboutMe from "./Components/AboutMe";
 
 function App() {
-  return <Navigation />;
+  return (
+    <>
+      <Navigation />
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/" element={<AboutMe />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
 }
 
 export default App;
--- my-react-portfolio/src/Components/AboutMe/index.js
+++ my-react-portfolio/src/Components/AboutMe/index.js
@@ -1,11 +1,48 @@
 import React from "react";
 import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/Row";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
 const AboutMe = () => {
   return (
     <Container fluid className="center" style={{ background: "" }}>
-      <Row className="" style={{}}></Row>
+      <Row className="justify-content-center" style={{}}>
+        <Card>
+          <Card.Img />
+          <Card.Body></Card.Body>
+        </Card>
+        {/* <img
+          src="\assets\images\IMG_3982 (2).jpg"
+          className="image justify-content-center"
+          alt="Developer"
+        />
+      </Row>
+
+      <Row className="text-center">
+        <h1>Courtney Bunting</h1>
+      </Row>
+
+      <Row className="text-center">
+        <h3>Full Stack Developer</h3>
+      </Row>
+
+      <Row className="text-center">
+        <p>
+          This is some information about me. blah blah blah blah blah blah blah
+          blah blah blah blah blah blah blah blah blah blah blah blah blah blah
+          blah blah blah blah blah blah blah blah blah blah blah blah blah blah
+          blah blah blah blah blah blah blah blah blah blah blah blah blah blah
+          blah blah blah blah blah blah blah blah blah blah blah blah blah blah
+          blah blah blah blah blah blah blah blah blah blah blah blah blah blah
+          blah blah blah blah blah blah blah blah blah blah blah blah blah blah
+          blah blah blah blah blah blah blah blah blah blah blah blah blah blah
+          blah blah blah blah blah blah blah blah blah blah blah blah blah blah
+          blah
+        </p> */}
+      </Row>
     </Container>
   );
 };
+
+export default AboutMe;
